8e668faaecd3237ef82ff6f627ebf3d66bb92beb
Daniel Njama Wangari <daniel.njama@prodapt.com>
2024-04-16 16:20:44 +0300

    more updates

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [ 
     path('',views.index,name='index'),
+    path('about/',views.about,name='about'),
+    path('courses/',views.courses,name='courses'),
+    path('teachers/',views.teachers,name='teachers'),
+    path('blog/',views.blog,name='blog'),
+    path('contact/',views.contact,name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -10,11 +10,11 @@ def about(request):
     return render(request,'about.html')
 
 def courses(request):
-    return render(request,'courses.html')
+    return render(request,'course.html')
 
 
 def teachers(request):
-    return render(request,'teachers.html')
+    return render(request,'teacher.html')
 
 def blog(request):
     return render(request,'blog.html')
